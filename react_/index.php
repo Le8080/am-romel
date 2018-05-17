f43fcf000801397d5b94b12816eccd8528a3ae68
Le8080 <leahfuentes.kd@gmail.com>
2018-05-18 02:52:08 +0800

    updates on react

--- react_/index.php
+++ react_/index.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://unpkg.com/react@16/umd/react.development.js"></script>
+    <!-- <script src="https://unpkg.com/react@16/umd/react.development.js"></script>
     <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-    <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+    <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script> -->
     <title>React-Directory</title>
 </head>
 <body>
+    <?php require_once('directory.php'); 
+        $db = DBobject::DBInstance();
+        $mysqli = $db->getDBConnect(); 
+        $mysqli = $db->get_records('SELECT  * FROM books where name like (:name)',array('name'=>"'%i%'",'id'=>'2','dsad'=>4));
+        di($mysqli);
+    ?>
     <div id="react-container"></div>
     <script type="text/babel">
 
